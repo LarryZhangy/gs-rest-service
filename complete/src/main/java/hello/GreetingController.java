ebeadba5728bf8f0fc1ccc6dab292414c43840c7
zhangyong <272517751@qq.com>
2018-09-25 10:54:20 +0000

    use sentry

--- complete/src/main/java/hello/GreetingController.java
+++ complete/src/main/java/hello/GreetingController.java
@@ -5,14 +5,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+import org.slf4j.MarkerFactory;
+
 @RestController
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+    private static final Logger logger = LoggerFactory.getLogger(GreetingController.class);
+
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+	logger.error("Get a greeting!");
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
